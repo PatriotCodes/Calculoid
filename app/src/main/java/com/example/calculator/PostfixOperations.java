fe528e80b26c699975e534a591a6937bd7941fe1
patriot <patriot130690@gmail.com>
2016-10-12 21:41:59 +0300

    license information added

--- app/src/main/java/com/example/calculator/PostfixOperations.java
+++ app/src/main/java/com/example/calculator/PostfixOperations.java
@@ -1,3 +1,22 @@
+/*
+Copyright (c) 2016 Alexander Triukhan
+Calculoid: A simple android calculator application with operation priority and memory functions.
+
+This file is part of Calculoid.
+
+Calculoid is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.example.calculator;
 
 import android.widget.TextView;
@@ -6,9 +25,7 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Stack;
 
-/**
- * Created by Александр on 11.05.2016.
- */
+
 public class PostfixOperations {
 
     private static final int MaxFractionDig = 10;
--- app/src/main/java/com/example/calculator/ValidityCheckers.java
+++ app/src/main/java/com/example/calculator/ValidityCheckers.java
@@ -1,8 +1,25 @@
+/*
+Copyright (c) 2016 Alexander Triukhan
+Calculoid: A simple android calculator application with operation priority and memory functions.
+
+This file is part of Calculoid.
+
+Calculoid is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.example.calculator;
 
-/**
- * Created by Александр on 11.05.2016.
- */
+
 public class ValidityCheckers {
 
     public static StringBuilder checkBrackets(String input) {
